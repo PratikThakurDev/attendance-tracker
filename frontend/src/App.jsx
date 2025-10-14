23669439810fae234199ce7e008b0891899bb0b6
PratikThakurDev <149695885+PratikThakurDev@users.noreply.github.com>
2025-10-14 09:35:19 +0000

    did the setup for the project

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import React from 'react'
+
+function App() {
+  return <>
+  <h1 className="grid place-content-center h-screen">
+    Hello world!
+  </h1>
+  </>
+}
+
+export default App
